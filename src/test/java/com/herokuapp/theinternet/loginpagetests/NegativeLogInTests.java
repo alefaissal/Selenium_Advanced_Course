4f479f780320e0109f424858afdfc8d4384f746f
Alex Faissal <alefaissal@gmail.com>
2021-02-04 15:00:24 -0800

    Fixed Final version

--- src/test/java/com/herokuapp/theinternet/loginpagetests/NegativeLogInTests.java
+++ src/test/java/com/herokuapp/theinternet/loginpagetests/NegativeLogInTests.java
@@ -1,21 +1,28 @@
 package com.herokuapp.theinternet.loginpagetests;
 
-//import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.Map;
+
 import org.testng.Assert;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.herokuapp.theinternet.base.CsvDataProviders;
 import com.herokuapp.theinternet.base.TestUtilities;
 import com.herokuapp.theinternet.pages.LoginPage;
 import com.herokuapp.theinternet.pages.WelcomePage;
 
 public class NegativeLogInTests extends TestUtilities {
 
-
-	@Parameters({ "username", "password", "expectedMessage" })
-	@Test(priority = 1)
-	public void negativeTest(String username, String password, String expectedErrorMessage) {
-		log.info("Starting negativeTest");
+	
+	@Test(priority = 1, dataProvider = "csvReader", dataProviderClass = CsvDataProviders.class)
+	public void negativeLogInTest(Map<String, String> testData) {
+		// Data
+		String no = testData.get("no");
+		String username  = testData.get("username");
+		String password = testData.get("password");
+		String expectedErrorMessage = testData.get("expectedMessage");
+		String description = testData.get("description");
+		
+		log.info("Starting negativeLogInTest #" + no + " for " + description);
 
 		// open main page
 		WelcomePage welcomePage = new WelcomePage(driver, log);
@@ -24,18 +31,14 @@ public class NegativeLogInTests extends TestUtilities {
 		// Click on Form Authentication link
 		LoginPage loginPage = welcomePage.clickFormAuthenticationLink();
 
-		// Execute Negative Log In
+		// execute negative login
 		loginPage.negativeLogIn(username, password);
-		
-		//Wait for error message
+
+		// wait for error message
 		loginPage.waitForErrorMessage();
-		
+		String message = loginPage.getErrorMessageText();
+
 		// Verification
-		String actualErrorMessage = loginPage.getErrorMessageText();
-		Assert.assertTrue(actualErrorMessage.contains(expectedErrorMessage),
-				"actualErrorMessage does not contain expectedErrorMessage\nexpectedErrorMessage: "
-						+ expectedErrorMessage + "\nactualErrorMessage: " + actualErrorMessage);
+		Assert.assertTrue(message.contains(expectedErrorMessage), "Message doesn't contain expected text.");
 	}
-
-
 }
--- src/test/java/com/herokuapp/theinternet/loginpagetests/PositiveLogInTests.java
+++ src/test/java/com/herokuapp/theinternet/loginpagetests/PositiveLogInTests.java
@@ -1,5 +1,6 @@
 package com.herokuapp.theinternet.loginpagetests;
 
+import org.openqa.selenium.Cookie;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,24 +13,36 @@ public class PositiveLogInTests extends TestUtilities {
 
 	@Test
 	public void logInTest() {
-		log.info("Starting logIn test");
-
 		// open main page
 		WelcomePage welcomePage = new WelcomePage(driver, log);
 		welcomePage.openPage();
+		takeScreenshot("WelcomePage opened");
 
 		// Click on Form Authentication link
 		LoginPage loginPage = welcomePage.clickFormAuthenticationLink();
-
-		// Execute Log In
+		takeScreenshot("LoginPage opened");
+		
+		// Add new cookie
+		Cookie ck = new Cookie("username", "tomsmith", "the-internet.herokuapp.com", "/", null);
+		loginPage.setCookie(ck);
+		
+		// execute log in
 		SecureAreaPage secureAreaPage = loginPage.logIn("tomsmith", "SuperSecretPassword!");
-
-		// verifications
-		// new url
+		takeScreenshot("SecureAreaPage opened");
+		
+		// Get cookies
+		String username = secureAreaPage.getCookie("username");
+		log.info("Username cookie: " + username);
+		String session = secureAreaPage.getCookie("rack.session");
+		log.info("Session cookie: " + session);
+		
+		
+		// Verifications
+		// New page url is expected
 		Assert.assertEquals(secureAreaPage.getCurrentUrl(), secureAreaPage.getPageUrl());
 
 		// log out button is visible
-		Assert.assertTrue(secureAreaPage.isLogOutButtonVisible(), "logOutButton is not visible.");
+		Assert.assertTrue(secureAreaPage.isLogOutButtonVisible(), "LogOut Button is not visible.");
 
 		// Successful log in message
 		String expectedSuccessMessage = "You logged into a secure area!";
@@ -37,6 +50,5 @@ public class PositiveLogInTests extends TestUtilities {
 		Assert.assertTrue(actualSuccessMessage.contains(expectedSuccessMessage),
 				"actualSuccessMessage does not contain expectedSuccessMessage\nexpectedSuccessMessage: "
 						+ expectedSuccessMessage + "\nactualSuccessMessage: " + actualSuccessMessage);
-
 	}
 }
